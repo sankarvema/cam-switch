5f6565a42abf815e0dab434b3c3c59f568307ec0
unknown <svema@CSCINDAH494415.asiapac.globalcsc.net>
2014-10-02 17:16:14 +0530

    Initial code with skeleton

---
+++ app/src/androidTest/java/sanstech/camswitch/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sanstech.camswitch;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
